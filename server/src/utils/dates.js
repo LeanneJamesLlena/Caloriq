24ea806c3ee22c74703bc1d0753f86871b26b3da
Leanne13430 <Leanne.Llena@student.lut.fi>
2025-10-28 15:20:03 +0200

    utils/nutrition.js added: logics: scales per 100 g food profile to actual weight functon and add two nutrient objects together function

--- server/src/utils/dates.js
+++ server/src/utils/dates.js
@@ -2,6 +2,7 @@
 import { DateTime } from 'luxon';
 import { config } from '../config/env.js';
 
+//Guarantees all diary entrees use consistent YYYY-MM-DD format
 export function toYMD(d = new Date(), tz = config.DEFAULT_TZ || 'Europe/Helsinki') {
     if (typeof d === 'string' && /^\d{4}-\d{2}-\d{2}$/.test(d)) return d;
     return DateTime.fromJSDate(new Date(d), { zone: tz }).toFormat('yyyy-LL-dd');
